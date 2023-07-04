90be0b8f9db3239c2bc1a5e0b7a5f253aa02b4df
Logan Roelofs <34667264+Logan-Roelofs@users.noreply.github.com>
2023-07-04 13:39:34 -0500

    Create kalirepo.sh
    kali repos

---
+++ kalirepo.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+# Update system
+echo "${GREEN}Updating system...${NC}"
+apt update > /dev/null 2>&1
+
+echo "${GREEN}Upgrading system...${NC}"
+apt upgrade -y > /dev/null 2>&1
+
+# Add Kali Linux repos to apt source
+echo "${GREEN}Adding Kali Linux repositories...${NC}"
+echo "deb http://http.kali.org/kali kali-rolling main contrib non-free" | tee /etc/apt/sources.list.d/kali.list > /dev/null
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 44C6513A8E4FB3D30875F758ED444FF07D8D0BF6 > /dev/null 2>&1
+apt-key --keyring /etc/apt/trusted.gpg.d/kali-archive-keyring.gpg adv --keyserver keyserver.ubuntu.com --recv-keys 44C6513A8E4FB3D30875F758ED444FF07D8D0BF6 > /dev/null 2>&1
+
+# Hold back all packages that are ready to be upgraded
+echo "${GREEN}Holding back kali's default packages...${NC}"
+packages=$(apt list --upgradable 2>/dev/null | tail -n +2 | cut -d'/' -f1)
+for package in $packages; do
+    apt-mark hold "$package"
+done
+
+echo "${GREEN}All steps completed successfully.${NC}"
+

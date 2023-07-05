6ab498c762549b173743b03815f13fda8230b013
Logan Roelofs <34667264+Logan-Roelofs@users.noreply.github.com>
2023-07-05 16:24:16 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,11 +30,42 @@ apt update > /dev/null 2>&1
 
 # Hold back specific packages
 echo "${GREEN}Holding back specific packages...${NC}"
-packages=$("base-files" "bluez" "bluez-obexd" "desktop-base" "firmware-misc-nonfree" "gir1.2-adw-1" "gir1.2-gtk-4.0" "gir1.2-polkit-1.0" "gnome-control-center" "gnome-control-center-data" "gnome-shell" "gnome-shell-common" "gnome-shell-extension-prefs" "gnome-shell-extensions" "gnome-terminal" "gnome-terminal-data" "init" "init-system-helpers" "libadwaita-1-0" "libbluetooth3" "libglib2.0-0" "libglib2.0-bin" "libglib2.0-data" "libgtk-4-1" "libgtk-4-bin" "libgtk-4-common" "libnautilus-extension4" "libpolkit-agent-1-0" "libpolkit-gobject-1-0" "linux-image-amd64" "mutter-common" "nautilus" "nautilus-data" "nautilus-extension-gnome-terminal" "pkexec" "polkitd")
-for package in "${packages[@]}"; do
-    echo "Holding package: $package"
-    apt-mark hold "$package"
-done
+apt-mark hold base-files
+apt-mark hold bluez
+apt-mark hold bluez-obexd
+apt-mark hold desktop-base
+apt-mark hold firmware-misc-nonfree
+apt-mark hold gir1.2-adw-1
+apt-mark hold gir1.2-gtk-4.0
+apt-mark hold gir1.2-polkit-1.0
+apt-mark hold gnome-control-center
+apt-mark hold gnome-control-center-data
+apt-mark hold gnome-shell
+apt-mark hold gnome-shell-common
+apt-mark hold gnome-shell-extension-prefs
+apt-mark hold gnome-shell-extensions
+apt-mark hold gnome-terminal
+apt-mark hold gnome-terminal-data
+apt-mark hold init
+apt-mark hold init-system-helpers
+apt-mark hold libadwaita-1-0
+apt-mark hold libbluetooth3
+apt-mark hold libglib2.0-0
+apt-mark hold libglib2.0-bin
+apt-mark hold libglib2.0-data
+apt-mark hold libgtk-4-1
+apt-mark hold libgtk-4-bin
+apt-mark hold libgtk-4-common
+apt-mark hold libnautilus-extension4
+apt-mark hold libpolkit-agent-1-0
+apt-mark hold libpolkit-gobject-1-0
+apt-mark hold linux-image-amd64
+apt-mark hold mutter-common
+apt-mark hold nautilus
+apt-mark hold nautilus-data
+apt-mark hold nautilus-extension-gnome-terminal
+apt-mark hold pkexec
+apt-mark hold polkitd
 
 
 # Get the current directory using pwd

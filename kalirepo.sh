06b05908982ce10c3e66639df0536301dc73b570
Logan Roelofs <34667264+Logan-Roelofs@users.noreply.github.com>
2023-07-05 15:53:09 -0500

    Update kalirepo.sh

--- kalirepo.sh
+++ kalirepo.sh
@@ -21,11 +21,58 @@ echo "${GREEN}Updating system...${NC}"
 apt update > /dev/null 2>&1
 
 # Hold back all packages that are ready to be upgraded
-echo "${GREEN}Holding back kali's default packages...${NC}"
-packages=$(apt list --upgradable 2>/dev/null | tail -n +2 | cut -d'/' -f1)
-for package in $packages; do
+
+#echo "${GREEN}Holding back kali's default packages...${NC}"
+#packages=$(apt list --upgradable 2>/dev/null | tail -n +2 | cut -d'/' -f1)
+#for package in $packages; do
+#    apt-mark hold "$package"
+#done
+
+# Hold back specific packages
+echo "${GREEN}Holding back specific packages...${NC}"
+packages=(
+    base-files
+    bluez
+    bluez-obexd
+    desktop-base
+    firmware-misc-nonfree
+    gir1.2-adw-1
+    gir1.2-gtk-4.0
+    gir1.2-polkit-1.0
+    gnome-control-center
+    gnome-control-center-data
+    gnome-shell
+    gnome-shell-common
+    gnome-shell-extension-prefs
+    gnome-shell-extensions
+    gnome-terminal
+    gnome-terminal-data
+    init
+    init-system-helpers
+    libadwaita-1-0
+    libbluetooth3
+    libglib2.0-0
+    libglib2.0-bin
+    libglib2.0-data
+    libgtk-4-1
+    libgtk-4-bin
+    libgtk-4-common
+    libnautilus-extension4
+    libpolkit-agent-1-0
+    libpolkit-gobject-1-0
+    linux-image-amd64
+    mutter-common
+    nautilus
+    nautilus-data
+    nautilus-extension-gnome-terminal
+    pkexec
+    polkitd
+)
+for package in "${packages[@]}"; do
     apt-mark hold "$package"
 done
+
+
 
 # Get the current directory using pwd
 current_directory=$(pwd)

0d5216e4460a1411485a813d423b2fe360d1c772
Your Name <you@example.com>
2023-07-09 18:42:26 -0500

    update

--- postinstall.sh
+++ postinstall.sh
@@ -22,7 +22,7 @@ apt-get install -y wireshark
 
 # Past the following into the terminal 
 echo "${GREEN}Copy and past the following into the terminal...${NC}"
-echo "${Yellow}gsettings set org.gnome.desktop.background picture-uri file:///usr/local/share/backgrounds/wallpaper.jpg && gsettings set org.gnome.desktop.background picture-uri-dark file:///usr/local/share/backgrounds/wallpaper.jpg && gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark' && gsettings set org.gnome.shell favorite-apps \"['com.brave.Browser.desktop', 'com.gexperts.Tilix.desktop', 'code_code.desktop', 'com.spotify.Client.desktop', 'md.obsidian.Obsidian.desktop', 'com.mattjakeman.ExtensionManager.desktop', 'com.valvesoftware.Steam.desktop']\"${NC}"
+echo "${Yellow}gsettings set org.gnome.desktop.background picture-uri file:///usr/local/share/backgrounds/wallpaper.jpg && gsettings set org.gnome.desktop.background picture-uri-dark file:///usr/local/share/backgrounds/wallpaper.jpg && gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark' && gsettings set org.gnome.shell favorite-apps \"['com.brave.Browser.desktop', 'com.gexperts.Tilix.desktop', 'code_code.desktop', 'com.spotify.Client.desktop', 'com.mattjakeman.ExtensionManager.desktop', 'com.valvesoftware.Steam.desktop']\"${NC}"
 
 
 # Remove automatically installed programs that are no longer required
